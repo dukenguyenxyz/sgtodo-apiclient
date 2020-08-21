1bc62f043635eb4b38928e4e08b252f781abeece
Duke <duke.raphael.ng@gmail.com>
2020-08-21 11:07:50 +1000

    refactor: clean up and refactor code

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,70 +1,74 @@
-require "spec"    # For testing
-require "webmock" # For mocking
-
+require "spec"             # For testing
 require "../src/apiclient" # Main code
-
-# Mocking
 require "../src/config.cr"
 require "../src/constants.cr"
 
-# Mock data
+# Posting data body
 BODY1 = {"title" => "Commute to work", "completed" => false}
+
+BODY2 = {"title" => "Finish spider-gazelle to do app", "completed" => true}
+
+BODY3 = {"title" => "Go hiking", "completed" => false}
+
+# # MOCKING: Comment out the whole following code if you want to disable mocking data and use the live url for testing
+
+require "webmock" # Mocking
+
+# Mock data
 returned_body1 = {
   "id":        235,
   "title":     "Commute to work",
   "completed": true,
   "order":     nil,
-  "url":       "#{App::SERVER_URI}/235",
+  "url":       "#{url(235.to_s)}",
 }
 
-BODY2 = {"title" => "Finish spider-gazelle to do app", "completed" => true}
 returned_body2 = {
   "id":        234,
   "title":     "Finish spider-gazelle to do app",
   "completed": true,
   "order":     nil,
-  "url":       "#{App::SERVER_URI}/234",
+  "url":       "#{url(234.to_s)}",
 }
 
-BODY3 = {"title" => "Go hiking", "completed" => false}
 returned_body3 = {
   "id":        236,
   "title":     "Go hiking",
   "completed": true,
   "order":     nil,
-  "url":       "#{App::SERVER_URI}/236",
+  "url":       "#{url(236.to_s)}",
 }
 
 task_id = returned_body1["id"].to_s
 
 # Set up testing mock
-WebMock.stub(:post, "#{App::SERVER_URI}")
+WebMock.stub(:post, url())
   .with(body: BODY1.to_json)
   .to_return(status: 201, body: returned_body1.to_json)
 
 # Mocking configs
 
 # index
-WebMock.stub(:get, "#{App::SERVER_URI}")
+WebMock.stub(:get, url())
   .to_return(body: [returned_body1, returned_body2].to_json)
 
 # show
-WebMock.stub(:get, "#{App::SERVER_URI}/#{task_id}")
+WebMock.stub(:get, url(task_id))
   .to_return(body: returned_body1.to_json)
 
 # destroy_all
-WebMock.stub(:delete, "#{App::SERVER_URI}")
+WebMock.stub(:delete, url())
   .to_return(body: ({} of String => String).to_json)
 
 # destroy
-WebMock.stub(:delete, "#{App::SERVER_URI}/#{task_id}")
+WebMock.stub(:delete, url(task_id))
   .to_return(body: ({} of String => String).to_json)
 
 # create
-WebMock.stub(:post, "#{App::SERVER_URI}")
+WebMock.stub(:post, url())
   .with(body: BODY2.to_json)
   .to_return(status: 201, body: returned_body2.to_json)
 
 # update
-WebMock.stub(:patch, "#{App::SERVER_URI}/#{task_id}")
+WebMock.stub(:patch, url(task_id))
   .to_return(body: returned_body3.to_json)
